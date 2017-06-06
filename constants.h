730a6960f2f29a811f719c7e6e5668b01bf5794f
Giuseppe <user@instant-contiki.(none)>
2017-06-06 12:15:53 +0200

    broker reimplemented, some changes in constants.h

--- constants.h
+++ constants.h
@@ -5,46 +5,68 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-typedef nx_struct conn_msg {
+typedef nx_struct {
 	nx_uint8_t msg_type;
 } conn_msg_t;
 
-typedef nx_struct sub_msg {
+typedef nx_struct{
+	nx_uint8_t msg_type;
+	nx_uint16_t msg_id;
+	nx_uint8_t topic;
+	nx_uint16_t data;
+	nx_uint8_t dupflag; //duplicate flag, set to true if the message is resent because the
+			            //broker didn't acknowledge the original msg. Useful when QoS > 0
+} pub_msg_t;
+
+typedef nx_struct {
 	nx_uint8_t msg_type;
 	nx_uint16_t msg_id;
 	nx_uint8_t topic;
 	nx_uint8_t qos;
 } sub_msg_t;
 
-typedef nx_struct pub_msg {
-	nx_uint8_t msg_type;
-	nx_uint16_t msg_id;
-	nx_uint8_t topic;
-	nx_uint8_t qos;
-	nx_uint16_t data;
-} pub_msg_t;
-
-typedef nx_struct data_msg {
-	nx_uint8_t msg_type;
-	nx_uint16_t msg_id;
-	nx_uint8_t topic;
-	nx_uint16_t data;
-} data_msg_t;
-
-enum{
-	//AM type
-	AM_MY_MSG = 6,
-	//quality of service
-	LOWQ = 0,
-	HIGHQ = 1,
-	//topics:
-	TEMPERATURE = 2,
-	HUMIDITY = 3,
-	LUMINOSITY = 4,
-	//msg types
-	SUBSCRIBE = 7,
-	PUBLISH = 8,
-	CONNECT = 9
+//quality of service
+enum{	
+	LOWQ,
+	HIGHQ,
+	NOT_SUB; //used in subscription matrix to represent a missing subscription
+	NOT_CONN; //used in subscription matrix to represent an disconneted client
 };
 
+//topics
+enum{
+	TEMPERATURE,
+	HUMIDITY,
+	LUMINOSITY,
+	NUM_OF_TOPICS
+};
+
+//message types
+enum{
+    CONNECT,
+	SUBSCRIBE,
+	PUBLISH,
+	UNSUBSCRIBE,
+	DISCONNECT,
+	/*
+	CONNACK,
+	SUBACK,
+	PUBACK,
+	UNSUBACK
+	*/
+};
+
+//general_constants
+enum{
+    AM_MY_MSG = 6,
+    MAX_CLIENTS = 8
+};
+
+/*
+enum{
+	OK,
+	UNACCEPTABLE,
+	ID_REJECTED,
+	SERVER_UNAVAILABLE
+};*/
 #endif
